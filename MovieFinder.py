4a388b6f2c03e48a17f55f8d547836ee4d2436fc
bugdaryan <bugdaryan@gmail.com>
2022-05-25 02:19:28 +0400

    Add find by title movie query

--- MovieFinder.py
+++ MovieFinder.py
@@ -2,6 +2,7 @@ from MovieAPIFinder import MovieAPIFinder
 from glob import glob
 import json
 import config
+from pymongo import MongoClient
 
 
 class MovieFinder:
@@ -22,6 +23,18 @@ class MovieFinder:
 
     
     def find_movie(self, top_movie_num=None, **kwargs):
-
+# ['input', 'search_by', 'min_year', 'max_year', 'min_rating', 'limit', 'fuzzy']
         if top_movie_num > 0:
             self.found_movie = self.top_movies[top_movie_num-1]
+        else:
+            if 'Title' in kwargs['search_by']:
+                client = MongoClient(config.MONGODB_URI)
+                db = client.movies
+                movies = db.movies
+
+                results = movies.find({'title': kwargs['input']}) \
+                            .collation(config.case_insensitive_collation) \
+                            .limit(kwargs['limit'])
+
+                self.search_result = list(results)
+
