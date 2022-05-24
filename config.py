24a58f69e83f58dc3193b7f56906d2bdc4c9d22f
bugdaryan <bugdaryan@gmail.com>
2022-05-25 02:19:13 +0400

    Update config add mongo collation

--- config.py
+++ config.py
@@ -4,7 +4,9 @@ import os
 
 dotenv.load_dotenv()
 MOVIE_INFO_API = os.environ['MOVIE_INFO_API']
-MONGODB_URI = os.environ['MONGODB_URI']
+MONGODB_URI = os.environ['MONGODB_URI_ATLAS']
+
+case_insensitive_collation = { 'locale': 'en', 'strength': 2 }
 
 find_button_clicks = {
     -1:0,
