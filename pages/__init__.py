d8220bafe8f6370f79ce01ec0341ebfbc8b24514
bugdaryan <bugdaryan@gmail.com>
2022-05-26 05:55:12 +0400

    pdate __init__ files

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,3 +1,4 @@
 from pages.overview_page import overview_page
 from pages.find_movie_page import find_movie_page
-from pages.result_page import result_page
+from pages.results_page import results_page
+from pages.movie_page import movie_page
